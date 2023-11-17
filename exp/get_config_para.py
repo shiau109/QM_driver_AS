4c60ce5bbf5e696e3a56f02bd2f7512081a1417b
willy <willy30406@gmail.com>
2023-11-17 18:51:27 +0800

    Revert "Modify find qubit spec"
    This reverts commit e008606d390732a51a328cadcb86df06baf0c247.

--- exp/get_config_para.py
+++
@@ -1,8 +0,0 @@
-
-
-def get_IF( element_name, config ):
-   return config["elements"][element_name]["intermediate_frequency"]
-
-def get_offset( element_name, config ):
-    con_name, port_num = config["elements"][element_name]["singleInput"]["port"]
-    return config["controllers"][con_name]["analog_outputs"][port_num]
