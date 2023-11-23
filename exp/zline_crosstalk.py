276fe37bf93f8e2d133352190df90085408a9032
shiau109 <shiau109@gmail.com>
2023-11-23 22:17:13 +0800

    Testing z line cross talk, first get par from config

---
+++ exp/zline_crosstalk.py
@@ -0,0 +1,118 @@
+from qm.qua import *
+from qm.QuantumMachinesManager import QuantumMachinesManager
+from qm import SimulationConfig
+from configuration import *
+from qualang_tools.results import fetching_tool, progress_counter
+from qualang_tools.loops import from_array
+from qualang_tools.plot import interrupt_on_close
+from RO_macros import multiRO_declare, multiRO_measurement, multiRO_pre_save
+import matplotlib.pyplot as plt
+import warnings
+from scipy import signal
+from scipy.optimize import curve_fit
+from matplotlib.ticker import FuncFormatter
+from common_fitting_func import *
+warnings.filterwarnings("ignore")
+
+from config_par import get_offset
+
+def zline_crosstalk( flux_modify_range, prob_q_name:str, ro_element:str, z_line:list, flux_settle_time, config, qmm:QuantumMachinesManager, n_avg:int=100, simulate=True):
+    """
+    Mapping z offset crosstalk by qubit frequency 
+    """
+    a_min = -flux_modify_range
+    a_max = flux_modify_range
+    da = flux_modify_range/20
+
+    flux_1_range = np.arange(a_min, a_max + da / 2, da)  # + da/2 to add a_max to amplitudes
+    flux_2_range = np.arange(a_min, a_max + da / 2, da)  # + da/2 to add a_max to amplitudes
+
+    flux_len = len(flux_1_range)        
+    with program() as zc:
+
+    
+        iqdata_stream = multiRO_declare( ro_element )
+        n = declare(int) 
+        n_st = declare_stream()
+        dc_1 = declare(fixed) 
+        dc_2 = declare(fixed) 
+
+        with for_(n, 0, n < n_avg, n + 1):
+            
+            with for_(*from_array(dc_1, flux_1_range)):
+                set_dc_offset( z_line[0], "single", get_offset(z_line[0],config)+dc_1 )
+                with for_(*from_array(dc_2, flux_2_range)):
+
+                    set_dc_offset( z_line[1], "single", get_offset(z_line[1],config)+dc_2 )
+                    
+                    # Init
+                    align()
+                    wait(flux_settle_time) 
+                    
+                    # Opration
+                    play( "x180", prob_q_name )
+                    
+                    # Readout
+                    align()
+                    multiRO_measurement(iqdata_stream, ro_element)
+            save(n, n_st)
+        with stream_processing():
+            n_st.save("n")
+            multiRO_pre_save( iqdata_stream, ro_element, (flux_len,flux_len) )
+
+    if simulate:
+        simulation_config = SimulationConfig(duration=10_000)  # In clock cycles = 4ns
+        job = qmm.simulate(config, zc, simulation_config)
+        job.get_simulated_samples().con1.plot()
+        plt.show()
+    else:
+        qm = qmm.open_qm(config)
+        job = qm.execute(zc)
+
+        ro_ch_name = []
+        ro_ch_name.append(f"{ro_element}_I")
+        ro_ch_name.append(f"{ro_element}_Q")
+        data_list = ro_ch_name + ["iteration"]   
+        results = fetching_tool(job, data_list=data_list, mode="live")
+
+        fig, ax = plt.subplots(2)
+        interrupt_on_close(fig, job)
+        output_data = {}
+        while results.is_processing():
+            fetch_data = results.fetch_all()
+            
+            ax[0].cla()
+            ax[1].cla()
+            output_data[ro_element] = np.array([fetch_data[0], fetch_data[1]])
+
+            ax[0].pcolor(flux_1_range, flux_2_range, output_data[ro_element][0])
+            ax[1].pcolor(flux_1_range, flux_2_range, output_data[ro_element][1])
+    
+
+            iteration = fetch_data[-1]
+            # Progress bar
+            progress_counter(iteration, n_avg, start_time=results.get_start_time())      
+
+            plt.legend()
+            plt.tight_layout()
+            plt.pause(1) 
+        plt.show()      
+        qm.close()
+        return output_data
+
+
+
+if __name__=='__main__':
+    qmm = QuantumMachinesManager(host=qop_ip, port=qop_port, cluster_name=cluster_name, octave=octave_config)
+
+    from qualang_tools.units import unit
+    u = unit(coerce_to_integer=True)
+    n_avg = 100
+    q_id = 0
+    crosstalk_q_id = 1
+    flux_modify_range = 0.1
+    prob_q_name = f"q{q_id+1}_xy"
+    ro_element = f"rr{q_id+1}"
+    z_line = [f"q{q_id+1}_z", f"q{crosstalk_q_id+1}_z"]
+    flux_settle_time = 0.1 * u.us
+    zline_crosstalk( flux_modify_range, prob_q_name, ro_element, z_line, flux_settle_time, config, qmm, n_avg, simulate=True)
