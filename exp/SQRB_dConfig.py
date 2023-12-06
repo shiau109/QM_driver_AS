a6b29bf8cf27bc4f3958ca9f8145c4011c7156b6
shiau109 <shiau109@gmail.com>
2023-12-06 10:29:22 +0800

    Update SQRB_dConfig.py

--- exp/SQRB_dConfig.py
+++ exp/SQRB_dConfig.py
@@ -35,6 +35,7 @@ from qualang_tools.units import unit
 
 from RO_macros import multiRO_declare, multiRO_measurement, multiRO_pre_save
 
+from matplotlib.figure import Figure
 
 #######################
 # AUXILIARY FUNCTIONS #
@@ -50,7 +51,8 @@ u = unit(coerce_to_integer=True)
 def power_law(power, a, b, p):
     return a * (p**power) + b
 
-
+# List of recovery gates from the lookup table
+inv_gates = [int(np.where(c1_table[i, :] == 0)[0][0]) for i in range(24)]
 def generate_sequence():
     cayley = declare(int, value=c1_table.flatten().tolist())
     inv_list = declare(int, value=inv_gates)
@@ -71,81 +73,81 @@ def generate_sequence():
     return sequence, inv_gate
 
 
-def play_sequence(sequence_list, depth, qubit):
+def play_sequence(sequence_list, depth, q_name):
     i = declare(int)
     with for_(i, 0, i <= depth, i + 1):
         with switch_(sequence_list[i], unsafe=True):
             with case_(0):
-                wait(pi_len // 4, f"q{qubit}_xy")
+                wait(pi_len // 4, q_name)
             with case_(1):
-                play("x180", f"q{qubit}_xy")
+                play("x180", q_name)
             with case_(2):
-                play("y180", f"q{qubit}_xy")
+                play("y180", q_name)
             with case_(3):
-                play("y180", f"q{qubit}_xy")
-                play("x180", f"q{qubit}_xy")
+                play("y180", q_name)
+                play("x180", q_name)
             with case_(4):
-                play("x90", f"q{qubit}_xy")
-                play("y90", f"q{qubit}_xy")
+                play("x90", q_name)
+                play("y90", q_name)
             with case_(5):
-                play("x90", f"q{qubit}_xy")
-                play("-y90", f"q{qubit}_xy")
+                play("x90", q_name)
+                play("-y90", q_name)
             with case_(6):
-                play("-x90", f"q{qubit}_xy")
-                play("y90", f"q{qubit}_xy")
+                play("-x90", q_name)
+                play("y90", q_name)
             with case_(7):
-                play("-x90", f"q{qubit}_xy")
-                play("-y90", f"q{qubit}_xy")
+                play("-x90", q_name)
+                play("-y90", q_name)
             with case_(8):
-                play("y90", f"q{qubit}_xy")
-                play("x90", f"q{qubit}_xy")
+                play("y90", q_name)
+                play("x90", q_name)
             with case_(9):
-                play("y90", f"q{qubit}_xy")
-                play("-x90", f"q{qubit}_xy")
+                play("y90", q_name)
+                play("-x90", q_name)
             with case_(10):
-                play("-y90", f"q{qubit}_xy")
-                play("x90", f"q{qubit}_xy")
+                play("-y90", q_name)
+                play("x90", q_name)
             with case_(11):
-                play("-y90", f"q{qubit}_xy")
-                play("-x90", f"q{qubit}_xy")
+                play("-y90", q_name)
+                play("-x90", q_name)
             with case_(12):
-                play("x90", f"q{qubit}_xy")
+                play("x90", q_name)
             with case_(13):
-                play("-x90", f"q{qubit}_xy")
+                play("-x90", q_name)
             with case_(14):
-                play("y90", f"q{qubit}_xy")
+                play("y90", q_name)
             with case_(15):
-                play("-y90", f"q{qubit}_xy")
+                play("-y90", q_name)
             with case_(16):
-                play("-x90", f"q{qubit}_xy")
-                play("y90", f"q{qubit}_xy")
-                play("x90", f"q{qubit}_xy")
+                play("-x90", q_name)
+                play("y90", q_name)
+                play("x90", q_name)
             with case_(17):
-                play("-x90", f"q{qubit}_xy")
-                play("-y90", f"q{qubit}_xy")
-                play("x90", f"q{qubit}_xy")
+                play("-x90", q_name)
+                play("-y90", q_name)
+                play("x90", q_name)
             with case_(18):
-                play("x180", f"q{qubit}_xy")
-                play("y90", f"q{qubit}_xy")
+                play("x180", q_name)
+                play("y90", q_name)
             with case_(19):
-                play("x180", f"q{qubit}_xy")
-                play("-y90", f"q{qubit}_xy")
+                play("x180", q_name)
+                play("-y90", q_name)
             with case_(20):
-                play("y180", f"q{qubit}_xy")
-                play("x90", f"q{qubit}_xy")
+                play("y180", q_name)
+                play("x90", q_name)
             with case_(21):
-                play("y180", f"q{qubit}_xy")
-                play("-x90", f"q{qubit}_xy")
+                play("y180", q_name)
+                play("-x90", q_name)
             with case_(22):
-                play("x90", f"q{qubit}_xy")
-                play("y90", f"q{qubit}_xy")
-                play("x90", f"q{qubit}_xy")
+                play("x90", q_name)
+                play("y90", q_name)
+                play("x90", q_name)
             with case_(23):
-                play("-x90", f"q{qubit}_xy")
-                play("y90", f"q{qubit}_xy")
-                play("-x90", f"q{qubit}_xy")
+                play("-x90", q_name)
+                play("y90", q_name)
+                play("-x90", q_name)
 
-def amp_calibration( max_circuit_depth, delta_clifford, q_name:str, ro_element:list, config, qmm:QuantumMachinesManager, sequence_repeat:int=1, n_avg=100, state_discrimination:list=None, initialization_macro=None, simulate:bool=True ):
+def single_qubit_RB( max_circuit_depth, delta_clifford, q_name:str, ro_element:list, config, qmm:QuantumMachinesManager, sequence_repeat:int=1, n_avg=100, state_discrimination:list=None, initialization_macro=None, simulate:bool=True ):
     
     
     is_discriminated = False
@@ -202,7 +204,7 @@ def amp_calibration( max_circuit_depth, delta_clifford, q_name:str, ro_element:l
                         # The strict_timing ensures that the sequence will be played without gaps
                         with strict_timing_():
                             # Play the random sequence of desired depth
-                            play_sequence(sequence_list, depth, qubit)
+                            play_sequence(sequence_list, depth, q_name)
                         # Align the two elements to measure after playing the circuit.
                         align()
 
@@ -223,7 +225,7 @@ def amp_calibration( max_circuit_depth, delta_clifford, q_name:str, ro_element:l
 
         with stream_processing():
             m_st.save("iteration")
-            if state_discrimination:
+            if is_discriminated:
                 # saves a 2D array of depth and random pulse sequences in order to get error bars along the random sequences
                 state_st.boolean_to_int().buffer(n_avg).map(FUNCTIONS.average()).buffer(
                     gate_step
@@ -262,19 +264,19 @@ def amp_calibration( max_circuit_depth, delta_clifford, q_name:str, ro_element:l
         # Send the QUA program to the OPX, which compiles and executes it
         job = qm.execute(rb)
         # Get results from QUA program
-        if state_discrimination:
+        if is_discriminated:
             results = fetching_tool(job, data_list=["state_avg", "iteration"], mode="live")
         else:
             results = fetching_tool(job, data_list=["I_avg", "Q_avg", "iteration"], mode="live")
         # Live plotting
-        fig = plt.figure()
+        fig, ax = plt.subplots()
         interrupt_on_close(fig, job)  # Interrupts the job when closing the figure
         # data analysis
         x = np.arange(0, max_circuit_depth + 0.1, delta_clifford)
         x[0] = 1  # to set the first value of 'x' to be depth = 1 as in the experiment
         while results.is_processing():
             # data analysis
-            if state_discrimination:
+            if is_discriminated:
                 state_avg, iteration = results.fetch_all()
                 value_avg = state_avg
             else:
@@ -284,15 +286,13 @@ def amp_calibration( max_circuit_depth, delta_clifford, q_name:str, ro_element:l
             # Progress bar
             progress_counter(iteration, num_of_sequences, start_time=results.get_start_time())
             # Plot averaged values
+            plot_SQRB_live( x, value_avg, ax )
             plt.cla()
-            plt.plot(x, value_avg, marker=".")
-            plt.xlabel("Number of Clifford gates")
-            plt.ylabel("Sequence Fidelity")
-            plt.title("Single qubit RB")
             plt.pause(0.1)
 
+
         # At the end of the program, fetch the non-averaged results to get the error-bars
-        if state_discrimination:
+        if is_discriminated:
             results = fetching_tool(job, data_list=["state"])
             state = results.fetch_all()[0]
             value_avg = np.mean(state, axis=0)
@@ -302,69 +302,85 @@ def amp_calibration( max_circuit_depth, delta_clifford, q_name:str, ro_element:l
             I, Q = results.fetch_all()
             value_avg = np.mean(I, axis=0)
             error_avg = np.std(I, axis=0)
-        # data analysis
-        pars, cov = curve_fit(
-            f=power_law,
-            xdata=x,
-            ydata=value_avg,
-            p0=[0.5, 0.5, 0.9],
-            bounds=(-np.inf, np.inf),
-            maxfev=2000,
-        )
-        stdevs = np.sqrt(np.diag(cov))
-
-        print("#########################")
-        print("### Fitted Parameters ###")
-        print("#########################")
-        print(f"A = {pars[0]:.3} ({stdevs[0]:.1}), B = {pars[1]:.3} ({stdevs[1]:.1}), p = {pars[2]:.3} ({stdevs[2]:.1})")
-        print("Covariance Matrix")
-        print(cov)
-
-        one_minus_p = 1 - pars[2]
-        r_c = one_minus_p * (1 - 1 / 2**1)
-        r_g = r_c / 1.875  # 1.875 is the average number of gates in clifford operation
-        r_c_std = stdevs[2] * (1 - 1 / 2**1)
-        r_g_std = r_c_std / 1.875
-
-        print("#########################")
-        print("### Useful Parameters ###")
-        print("#########################")
-        print(
-            f"Error rate: 1-p = {np.format_float_scientific(one_minus_p, precision=2)} ({stdevs[2]:.1})\n"
-            f"Clifford set infidelity: r_c = {np.format_float_scientific(r_c, precision=2)} ({r_c_std:.1})\n"
-            f"Gate infidelity: r_g = {np.format_float_scientific(r_g, precision=2)}  ({r_g_std:.1})"
-        )
-
-        # Plots
-        plt.figure()
-        plt.errorbar(x, value_avg, yerr=error_avg, marker=".")
-        plt.plot(x, power_law(x, *pars), linestyle="--", linewidth=2)
-        plt.xlabel("Number of Clifford gates")
-        plt.ylabel("Sequence Fidelity")
-        plt.title("Single qubit RB")
-
-        # np.savez("rb_values", value)
 
         # Close the quantum machines at the end in order to put all flux biases to 0 so that the fridge doesn't heat-up
         qm.close()
 
+        return x, value_avg, error_avg
+
+
+def ana_SQRB( x, y ):
+    
+    pars, cov = curve_fit(
+        f=power_law,
+        xdata=x,
+        ydata=y,
+        p0=[0.5, 0.5, 0.9],
+        bounds=(-np.inf, np.inf),
+        maxfev=2000,
+    )
+    stdevs = np.sqrt(np.diag(cov))
+
+    print("#########################")
+    print("### Fitted Parameters ###")
+    print("#########################")
+    print(f"A = {pars[0]:.3} ({stdevs[0]:.1}), B = {pars[1]:.3} ({stdevs[1]:.1}), p = {pars[2]:.3} ({stdevs[2]:.1})")
+    print("Covariance Matrix")
+    print(cov)
+
+    one_minus_p = 1 - pars[2]
+    r_c = one_minus_p * (1 - 1 / 2**1)
+    r_g = r_c / 1.875  # 1.875 is the average number of gates in clifford operation
+    r_c_std = stdevs[2] * (1 - 1 / 2**1)
+    r_g_std = r_c_std / 1.875
+
+    print("#########################")
+    print("### Useful Parameters ###")
+    print("#########################")
+    print(
+        f"Error rate: 1-p = {np.format_float_scientific(one_minus_p, precision=2)} ({stdevs[2]:.1})\n"
+        f"Clifford set infidelity: r_c = {np.format_float_scientific(r_c, precision=2)} ({r_c_std:.1})\n"
+        f"Gate infidelity: r_g = {np.format_float_scientific(r_g, precision=2)}  ({r_g_std:.1})"
+    )
+    
+    return pars
+
+def plot_SQRB_live( x, y, ax ):
+    ax.plot(x, y, marker=".")
+    ax.xlabel("Number of Clifford gates")
+    ax.ylabel("Sequence Fidelity")
+    ax.title("Single qubit RB")
+
+def plot_SQRB_result( x, y, yerr, fig:Figure=None ):
+
+    fig, ax = plt.subplots()
+    par = ana_SQRB( x, y )
+    ax.errorbar(x, y, yerr=yerr, marker=".")
+    ax.plot(x, power_law(x, *par), linestyle="--", linewidth=2)
+    ax.set_xlabel("Number of Clifford gates")
+    ax.set_ylabel("Sequence Fidelity")
+    ax.set_title("Single qubit RB")
+
 
 
 if __name__ == '__main__':
-
+    qmm = QuantumMachinesManager(host=qop_ip, port=qop_port, cluster_name=cluster_name, octave=octave_config)
+    
     warnings.filterwarnings("ignore")
     from QM_config_dynamic import QM_config, Circuit_info
     dyna_config = QM_config()
     dyna_config.import_config(path=r'.\TEST\BETAsite\QM\OPXPlus\3_5q Tune up\Standard Configuration\Config_1205_Calied')
     the_specs = Circuit_info(q_num=5)
     the_specs.import_spec(path=r'.\TEST\BETAsite\QM\OPXPlus\3_5q Tune up\Standard Configuration\Spec_1205_Calied')
+    
     config = dyna_config.get_config()
     pi_len = the_specs.get_spec_forConfig('xy')['q1']['pi_len']
     ##############################
     # Program-specific variables #
     ##############################
-    qubit = 1
-    threshold = the_specs.get_spec_forConfig('ro')[f'q{qubit}']['ge_threshold']
+    q_name = "q1_xy"
+    ro_element = ["rr1","rr2","rr3","rr4"]
+    threshold = the_specs.get_spec_forConfig('ro')[q_name]['ge_threshold']
 
 
     num_of_sequences = 350  # Number of random sequences
@@ -374,6 +390,7 @@ if __name__ == '__main__':
     assert (max_circuit_depth / delta_clifford).is_integer(), "max_circuit_depth / delta_clifford must be an integer."
     seed = 345324  # Pseudo-random number generator seed
     # Flag to enable state discrimination if the readout has been calibrated (rotated blobs and threshold)
-    state_discrimination = True
-    # List of recovery gates from the lookup table
-    inv_gates = [int(np.where(c1_table[i, :] == 0)[0][0]) for i in range(24)]
+    state_discrimination = [1e-3]
+    x, value_avg, error_avg = single_qubit_RB( max_circuit_depth, delta_clifford, q_name, ro_element, config, qmm )
+    
+    plot_SQRB_result( x, value_avg, error_avg )
