fa74202da231ce17f18f5b1ed1c198c6e8d0b42c
shiau109 <shiau109@gmail.com>
2024-02-16 00:52:34 +0800

    Update flux_dep_resonator.py
    Change to xarray dataset output

--- src/exp/flux_dep_resonator.py
+++ src/exp/flux_dep_resonator.py
@@ -1,23 +1,4 @@
-"""
-        RESONATOR SPECTROSCOPY VERSUS FLUX
-This sequence involves measuring the resonator by sending a readout pulse and demodulating the signals to
-extract the 'I' and 'Q' quadratures. This is done across various readout intermediate frequencies and flux biases.
-The resonator frequency as a function of flux bias is then extracted and fitted so that the parameters can be stored in the configuration.
 
-This information can then be used to adjust the readout frequency for the maximum frequency point.
-
-Prerequisites:
-    - Calibration of the time of flight, offsets, and gains (referenced as "time_of_flight").
-    - Calibration of the IQ mixer connected to the readout line (be it an external mixer or an Octave port).
-    - Identification of the resonator's resonance frequency (referred to as "resonator_spectroscopy_multiplexed").
-    - Configuration of the readout pulse amplitude and duration.
-    - Specification of the expected resonator depletion time in the configuration.
-
-Before proceeding to the next node:
-    - Update the readout frequency, labeled as "resonator_IF", in the configuration.
-    - Adjust the flux bias to the maximum frequency point, labeled as "max_frequency_point", in the configuration.
-    - Update the resonator frequency versus flux fit parameters (amplitude_fit, frequency_fit, phase_fit, offset_fit) in the configuration
-"""
 
 from qm.qua import *
 from qm.QuantumMachinesManager import QuantumMachinesManager
@@ -31,20 +12,28 @@ from qualang_tools.units import unit
 u = unit(coerce_to_integer=True)
 
 warnings.filterwarnings("ignore")
+import xarray as xr
 
-def flux_dep_cavity( ro_element:list, config:dict, qm_machine:QuantumMachinesManager, n_avg:int=100, flux_settle_time_ns:int=1000, freq_span_MHz:float=3, flux_span:float=0.3, flux_resolu:float=0.015, freq_resolu_MHz:float=0.05, initializer:tuple=None ):
+def flux_dep_cavity( ro_element:list, config:dict, qm_machine:QuantumMachinesManager, n_avg:int=100, flux_settle_time_ns:int=1000, freq_span:float=3, flux_span:float=0.3, flux_resolu:float=0.015, freq_resolution:float=0.05, initializer:tuple=None ):
     """
     
     return is tuple
     1. data\n
     2. relative frequency (MHz) ref to original IF in config\n
-    3. absolute voltage for flux line\n
+    3. absolute voltage for fluxes line\n
 
     """
-    dfs = np.arange(-freq_span_MHz*u.MHz,(freq_span_MHz+freq_resolu_MHz)*u.MHz,freq_resolu_MHz*u.MHz)
-    flux = np.arange(-flux_span,flux_span+flux_resolu,flux_resolu)
-    df_len = dfs.shape[0]
-    flux_len = flux.shape[0]
+    freq_span_qua = freq_span * u.MHz
+    freq_resolution_qua = freq_resolution * u.MHz
+
+    freqs_qua = np.arange(-freq_span_qua/2,freq_span_qua/2,freq_resolution_qua)
+    fluxes = np.arange(-flux_span,flux_span+flux_resolu,flux_resolu)
+
+    freqs_mhz = freqs_qua/1e6 #  Unit in MHz
+
+    freqs_len = freqs_qua.shape[0]
+    flux_len = fluxes.shape[0]
+
     with program() as multi_res_spec_vs_flux:
         # QUA macro to declare the measurement variables and their corresponding streams for a given number of resonators
         iqdata_stream = multiRO_declare( ro_element )
@@ -52,15 +41,15 @@ def flux_dep_cavity( ro_element:list, config:dict, qm_machine:QuantumMachinesMan
         n = declare(int)
         n_st = declare_stream()
         df = declare(int)  # QUA variable for sweeping the readout frequency detuning around the resonance
-        dc = declare(fixed)  # QUA variable for sweeping the flux bias
+        dc = declare(fixed)  # QUA variable for sweeping the fluxes bias
 
         with for_(n, 0, n < n_avg, n + 1):
-            with for_(*from_array(df, dfs)):
+            with for_(*from_array(df, freqs_qua)):
                 for ro in ro_element:
                     resonator_IF = config['elements'][ro]["intermediate_frequency"]
                     update_frequency(ro, df + resonator_IF)
 
-                with for_(*from_array(dc, flux)):
+                with for_(*from_array(dc, fluxes)):
                     # initializaion
                     if initializer is None:
                         wait(1*u.us,ro_element)
@@ -83,7 +72,7 @@ def flux_dep_cavity( ro_element:list, config:dict, qm_machine:QuantumMachinesMan
 
         with stream_processing():
             n_st.save("n")
-            multiRO_pre_save( iqdata_stream, ro_element, (df_len, flux_len))
+            multiRO_pre_save( iqdata_stream, ro_element, (freqs_len, flux_len))
             
     #######################
 
@@ -104,19 +93,26 @@ def flux_dep_cavity( ro_element:list, config:dict, qm_machine:QuantumMachinesMan
         iteration = fetch_data[-1]
         # Progress bar
         progress_counter(iteration, n_avg, start_time=results.get_start_time()) 
-    # Close the quantum machines at the end in order to put all flux biases to 0 so that the fridge doesn't heat-up
+    # Close the quantum machines at the end in order to put all fluxes biases to 0 so that the fridge doesn't heat-up
     qm.close()
     
-    
-    relative_freq = dfs/1e6  # np.arange(-freq_span_MHz,(freq_span_MHz+freq_resolu_MHz),freq_resolu_MHz)
-    return output_data, relative_freq, flux
+    # Creating an xarray dataset
+    fetch_data = results.fetch_all()
+    for r_idx, r_name in enumerate(ro_element):
+        output_data[r_name] = ( ["mixer","flux","frequency"],
+                               np.array([fetch_data[r_idx*2], fetch_data[r_idx*2+1]]) )
+    dataset = xr.Dataset(
+        output_data,
+        coords={ "mixer":np.array(["I","Q"]), "frequency": freqs_mhz, "flux": fluxes }
+    )
+    return dataset
 
 
-def plot_flux_dep_resonator( data, dfs, flux, ax=None ):
+def plot_flux_dep_resonator( data, freqs_qua, fluxes, ax=None ):
     """
     data shape ( 2, N, M )
     2 is I,Q
-    N is flux
+    N is fluxes
     M is freq
     """
     idata = data[0]
@@ -128,5 +124,5 @@ def plot_flux_dep_resonator( data, dfs, flux, ax=None ):
         fig, ax = plt.subplots()
         ax.set_title('pcolormesh')
         fig.show()
-    ax.pcolormesh(flux, dfs, np.abs(s21), cmap='RdBu')# , vmin=z_min, vmax=z_max)
+    ax.pcolormesh(fluxes, freqs_qua, np.abs(s21), cmap='RdBu')# , vmin=z_min, vmax=z_max)
     
