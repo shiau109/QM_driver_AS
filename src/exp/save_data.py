8401bee2ea991c8263d3de8a831e712739d3b1d8
shiau109 <shiau109@gmail.com>
2024-02-19 10:23:13 +0800

    Update save_data.py

--- src/exp/save_data.py
+++ src/exp/save_data.py
@@ -2,11 +2,21 @@ from datetime import datetime
 import sys
 import numpy as np
 
-def save_npz( save_dir, file_name, output_data, suffix_time:bool=True ):
-    save_path = f"{save_dir}\{file_name}"
+def save_npz( save_dir, file_name, output_data:dict, suffix_time:bool=True ):
+    save_path = f"{save_dir}/{file_name}"
     save_time = ""
     if suffix_time:
-        save_time = str(datetime.now().strftime("%Y%m%d-%H%M%S"))
+        save_time = str(datetime.now().strftime("%Y%m%d_%H%M"))
         save_time = "_"+save_time
     save_path = save_path+save_time+".npz" 
     np.savez(save_path, **output_data)
+
+import xarray as xr
+def save_nc( save_dir, file_name, output_data:xr.Dataset, suffix_time:bool=True ):
+    save_path = f"{save_dir}/{file_name}"
+    save_time = ""
+    if suffix_time:
+        save_time = str(datetime.now().strftime("%Y%m%d_%H%M"))
+        save_time = "_"+save_time
+    save_path = save_path+save_time+".nc" 
+    output_data.to_netcdf(save_path, **output_data)
