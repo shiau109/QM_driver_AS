15a3577c365648294c68e1cbd6b9b4b63586446e
shiau109 <shiau109@gmail.com>
2024-03-06 14:43:41 +0800

    separate path and buildup, extend buildup to multi OPX+ cluster
    Other import not change yet

---
+++ src/OnMachine/SetConfig/config_path.py
@@ -0,0 +1,10 @@
+from config_component.channel_info import ChannelInfo
+from config_component.configuration import Configuration
+import os
+SpecConfig_path = os.getcwd()+'/config/'
+########### For other import ################
+config_loca = SpecConfig_path+"DR4_5Q4C_config_new" #
+spec_loca = SpecConfig_path+"DR4_5Q4C_spec_new"     #
+#############################################
+
+
