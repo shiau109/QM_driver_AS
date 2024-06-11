363fb955d97b9488f9ef50e742909bb0723e1a06
shiau109 <shiau109@gmail.com>
2024-06-11 11:44:19 +0800

    Update S6_T1 spectrum.py
    Change output file default name

--- application/experiment_method/S6_T1 spectrum.py
+++ application/experiment_method/S6_T1 spectrum.py
@@ -10,38 +10,38 @@ config = config_obj.get_config()
 qmm, _ = spec.buildup_qmm()
 
 from ab.QM_config_dynamic import initializer
-init_macro = initializer(200000,mode='wait')
 
 from exp.save_data import save_nc, save_fig
-save_dir = link_config["path"]["output_root"]
+
 
 import matplotlib.pyplot as plt
 
 # Set parameters
-ro_elements = ["q0_ro"]
-q_name = ['q0_xy']
-z_name = ['q0_z']
+init_macro = initializer(300000,mode='wait')
+ro_elements = ["q0_ro", "q1_ro", "q2_ro", "q3_ro", "q4_ro"]
+q_name = ["q4_xy"]
+z_name = ['q4_z']
 
 save_data = True
+save_dir = link_config["path"]["output_root"]
 save_name = f"{q_name[0]}_T1spectrum"
 
-n_avg = 100
-max_time = 2 #us
-time_resolution = 0.008 #us
-flux_range = (-0.05, 0.05)
-flux_resolution = 0.005
+n_avg = 200
+max_time = 60 #us
+time_resolution = 0.6 #us
+flux_range = (-0.3, 0.3)
+flux_resolution = 0.0015
 
 from exp.z_pulse_relaxation_time import exp_z_pulse_relaxation_time
 dataset = exp_z_pulse_relaxation_time( max_time, time_resolution, flux_range, flux_resolution, q_name, z_name, ro_elements, config, qmm, n_avg=n_avg, initializer=init_macro)
 
-if save_data: save_nc(r"D:\Data\03205Q4C_6", f"{q_name}_T1_spectrum", dataset) 
+if save_data: save_nc(save_dir, save_name, dataset) 
 
 
 # Plot
 time = dataset.coords["time"].values
 flux = dataset.coords["z_voltage"].values
 
-from exp.relaxation_time import plot_T1
 for ro_name, data in dataset.data_vars.items():
     fig_0, ax_0 = plt.subplots()
     ax_0.plot(time, data.values[0][0])
@@ -53,7 +53,7 @@ for ro_name, data in dataset.data_vars.items():
     pcm = ax.pcolormesh( time/1000, flux, data.values[0], cmap='RdBu')# , vmin=z_min, vmax=z_max)
     plt.colorbar(pcm, label='Value')
 
-if save_data: save_fig(r"D:\Data\03205Q4C_6", f"{q_name}_T1_spectrum", dataset) 
+if save_data: save_fig( save_dir, save_name, dataset) 
 
 plt.show()
 
