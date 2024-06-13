ec8adf0d84e3d411c335e11a0167fdb226fae679
shiau109 <shiau109@gmail.com>
2024-06-13 21:28:22 +0800

    Cryoscope4ns Pass pulse sche test

--- src/exp/cryoscope.py
+++ src/exp/cryoscope.py
@@ -1,5 +1,5 @@
 """
-        CRYOSCOPE - 4ns
+        CRYOSCOPE with 4ns granularity
 The goal of this protocol is to measure the step response of the flux line and design proper FIR and IIR filters
 (implemented on the OPX) to pre-distort the flux pulses and improve the two-qubit gates fidelity.
 Since the flux line ends on the qubit chip, it is not possible to measure the flux pulse after propagation through the
@@ -28,25 +28,156 @@ but the step must be larger than 1 clock cycle (4ns) and the minimum pulse durat
 Prerequisites:
     - Having found the resonance frequency of the resonator coupled to the qubit under study (resonator_spectroscopy).
     - Having calibrated qubit gates (x90 and y90) by running qubit spectroscopy, rabi_chevron, power_rabi, Ramsey and updated the configuration.
+    - (optional) Having calibrated the readout to perform state discrimination (IQ_blobs).
 
 Next steps before going to the next node:
     - Update the FIR and IIR filter taps in the configuration (config/controllers/con1/analog_outputs/"filter": {"feedforward": fir, "feedback": iir}).
+    - WARNING: the digital filters will add a global delay --> need to recalibrate IQ blobs (rotation_angle & ge_threshold).
 """
 
-from qm.QuantumMachinesManager import QuantumMachinesManager
 from qm.qua import *
-from qm import SimulationConfig
-from configuration import *
+from qm import QuantumMachinesManager
+
+from qualang_tools.results import progress_counter, fetching_tool
+from qualang_tools.plot import interrupt_on_close
+from qualang_tools.loops import from_array
+
+from qualang_tools.units import unit
+u = unit(coerce_to_integer=True)
+
+from exp.QMMeasurement import QMMeasurement
+from exp.RO_macros import multiRO_declare, multiRO_measurement, multiRO_pre_save
+
 from scipy import signal, optimize
 import matplotlib.pyplot as plt
-from qualang_tools.results import fetching_tool, progress_counter
-from qualang_tools.plot import interrupt_on_close
-import numpy as np
-from macros import qua_declaration, multiplexed_readout
-from qualang_tools.loops import from_array
-import warnings
+import xarray as xr
 
-warnings.filterwarnings("ignore")
+class Cryoscope( QMMeasurement ):
+    """
+    Parameters:
+    Search cavities with the given IF range along the given ro_element's LO, (LO+freq_range[0],LO+freq_range[1]) .\n
+
+    freq_range:\n
+    is a tuple ( upper, lower ), Unit in MHz.\n
+    resolution:\n
+    unit in MHz.\n
+    ro_element: ["q1_ro"], temporarily support only 1 element in the list.\n
+    initializer: from `initializer(paras,mode='depletion')`, and use paras return from `Circuit_info.give_depletion_time_for()`  
+    Return: \n
+
+    """
+    def __init__( self, config, qmm: QuantumMachinesManager ):
+        super().__init__( config, qmm )
+
+        self.ro_elements = ["q0_ro"]
+        self.z_elements = []
+        self.xy_elements = []
+
+        self.initializer = None
+        
+        self.xy_z_timing_cc_qua = 20
+
+        self.time_range = ( 16, 600 )
+        self.resolution = 4
+        self.duration_cc_qua = self._lin_cc_array( )
+
+    
+
+    def _get_qua_program( self ):
+        
+        with program() as cryoscope:
+            n = declare(int)  # QUA variable for the averaging loop
+            t = declare(int)  # QUA variable for the flux pulse duration
+            flag = declare(bool)  # QUA boolean to switch between x90 and y90
+
+            iqdata_stream = multiRO_declare( self.ro_elements )
+
+            n_st = declare_stream()  # Stream for the averaging iteration 'n'
+
+            # Outer loop for averaging
+            with for_(n, 0, n < self.shot_num, n + 1):
+                # Loop over the truncated flux pulse
+                with for_(*from_array(t, self.duration_cc_qua)):
+                    # Alternate between X/2 and Y/2 pulses
+                    with for_each_(flag, [True, False]):
+
+                        # Initialization
+                        if self.initializer is None:
+                            wait( 100*u.us )
+                        else:
+                            try:
+                                self.initializer[0](*self.initializer[1])
+                            except:
+                                print("initializer didn't work!")
+                                wait( 100*u.us )
+
+
+
+                        # Operation
+                        # Play first X/2
+                        play("x90", self.xy_elements[0])
+                        # Play truncated flux pulse
+                        align()
+                        # Wait some time to ensure that the flux pulse will arrive after the x90 pulse
+                        wait( self.xy_z_timing_cc_qua )
+
+                        play("const", self.z_elements[0], duration=t)
+                        # Wait for the idle time set slightly above the maximum flux pulse duration to ensure that the 2nd x90
+                        # pulse arrives after the longest flux pulse
+                        wait( max(self.duration_cc_qua) +self.xy_z_timing_cc_qua, self.xy_elements[0] )
+                        # Play second X/2 or Y/2
+                        with if_(flag):
+                            play("x90", self.xy_elements[0])
+                        with else_():
+                            play("y90", self.xy_elements[0])
+
+                        # Measure resonator state after the sequence
+                        align()
+                        # Readout
+                        multiRO_measurement( iqdata_stream, self.ro_elements, weights='rotated_' )
+                save(n, n_st)
+
+            with stream_processing():
+                # Cast the data into a 1D vector, average the 1D vectors together and store the results on the OPX processor
+                multiRO_pre_save( iqdata_stream, self.ro_elements, (len(self.duration_cc_qua),))
+                n_st.save("iteration")
+
+        return cryoscope
+    
+    def _get_fetch_data_list( self ):
+        ro_ch_name = []
+        for r_name in self.ro_elements:
+            ro_ch_name.append(f"{r_name}_I")
+            ro_ch_name.append(f"{r_name}_Q")
+
+        data_list = ro_ch_name + ["iteration"]   
+        return data_list
+    
+    def _data_formation( self ):
+
+        time = self.duration_cc_qua *4
+        output_data = {}
+        for r_idx, r_name in enumerate(self.ro_elements):
+            output_data[r_name] = ( ["mixer","time", "r90"],
+                                np.array([self.fetch_data[r_idx*2], self.fetch_data[r_idx*2+1]]) )
+        dataset = xr.Dataset(
+            output_data,
+            coords={ "mixer":np.array(["I","Q"]), "time": time, "r90":np.array(["x","y"]) }
+        )
+        transposed_data = dataset.transpose("mixer", "r90", "time")
+
+        return transposed_data
+
+    def _lin_cc_array( self ):
+
+        cc_resolution = self.resolution//4
+
+        min_cc = self.time_range[0]//4
+        if min_cc < 4: min_cc = 4
+        max_cc = self.time_range[1]//4
+        
+        return np.arange( min_cc, max_cc, cc_resolution )
+    
 
 
 ####################
@@ -67,9 +198,9 @@ def exponential_correction(A, tau, Ts=1e-9):
     """Derive FIR and IIR filter taps based on the exponential coefficients A and tau from 1 + a * np.exp(-x / t).
 
     :param A: amplitude of the exponential decay.
-    :param tau: decay time of the exponential decay
-    :param Ts: sampling period. Default is 1e-9
-    :return: FIR and IIR taps
+    :param tau: decay time of the exponential decay.
+    :param Ts: sampling period. Default is 1e-9.
+    :return: FIR and IIR taps.
     """
     tau = tau * Ts
     k1 = Ts + 2 * tau * (A + 1)
@@ -104,226 +235,3 @@ def filter_calc(exponential):
     return feedforward_taps, feedback_taps
 
 
-###################
-# The QUA program #
-###################
-# Index of the qubit to measure
-qubit = 1
-
-
-n_avg = 10_000  # Number of averages
-# Flux pulse durations in clock cycles (4ns) - must be > 4 or the pulse won't be played.
-durations = np.arange(3, const_flux_len // 4, 1)  # Starts at 3 clock-cycles to have the first point without pulse.
-flux_waveform = np.array([const_flux_amp] * max(durations))
-xplot = durations * 4  # x-axis for plotting and deriving the filter taps - must be in ns.
-step_response_th = [1.0] * len(xplot)  # Perfect step response (square)
-
-
-with program() as cryoscope:
-    I, I_st, Q, Q_st, n, n_st = qua_declaration(nb_of_qubits=2)
-    t = declare(int)  # QUA variable for the flux pulse duration
-    flag = declare(bool)  # QUA boolean to switch between x90 and y90
-    state = [declare(bool) for _ in range(2)]
-    state_st = [declare_stream() for _ in range(2)]
-
-    # Outer loop for averaging
-    with for_(n, 0, n < n_avg, n + 1):
-        # Loop over the truncated flux pulse
-        with for_(*from_array(t, durations)):
-            # Alternate between X/2 and Y/2 pulses
-            with for_each_(flag, [True, False]):
-                # Play first X/2
-                play("x90", f"q{qubit}_xy")
-                # Play truncated flux pulse
-                align()
-                # Wait some time to ensure that the flux pulse will arrive after the x90 pulse
-                wait(20 * u.ns)
-                # Play the flux pulse only if t is larger than the minimum of 4 clock cycles (16ns)
-                with if_(t > 3):
-                    play("const", f"q{qubit}_z", duration=t)
-                # Wait for the idle time set slightly above the maximum flux pulse duration to ensure that the 2nd x90
-                # pulse arrives after the longest flux pulse
-                wait((len(flux_waveform) + 20) * u.ns, f"q{qubit}_xy")
-                # Play second X/2 or Y/2
-                with if_(flag):
-                    play("x90", f"q{qubit}_xy")
-                with else_():
-                    play("y90", f"q{qubit}_xy")
-                # Measure resonator state after the sequence
-                align()
-                multiplexed_readout(I, I_st, Q, Q_st, resonators=[1, 2], weights="rotated_")
-                # State discrimination
-                assign(state[0], I[0] > ge_threshold_q1)
-                assign(state[1], I[1] > ge_threshold_q2)
-                save(state[0], state_st[0])
-                save(state[1], state_st[1])
-                # Wait cooldown time and save the results
-                wait(thermalization_time * u.ns)
-        save(n, n_st)
-
-    with stream_processing():
-        # for the progress counter
-        n_st.save("n")
-        # resonator 1
-        I_st[0].buffer(2).buffer(len(durations)).average().save("I1")
-        Q_st[0].buffer(2).buffer(len(durations)).average().save("Q1")
-        state_st[0].boolean_to_int().buffer(2).buffer(len(durations)).average().save("state1")
-        # resonator 2
-        I_st[1].buffer(2).buffer(len(durations)).average().save("I2")
-        Q_st[1].buffer(2).buffer(len(durations)).average().save("Q2")
-        state_st[1].boolean_to_int().buffer(2).buffer(len(durations)).average().save("state2")
-
-#####################################
-#  Open Communication with the QOP  #
-#####################################
-
-qmm = QuantumMachinesManager(host=qop_ip, port=qop_port, cluster_name=cluster_name, octave=octave_config)
-
-###########################
-# Run or Simulate Program #
-###########################
-simulate = False
-
-if simulate:
-    # Simulates the QUA program for the specified duration
-    simulation_config = SimulationConfig(duration=10_000)  # In clock cycles = 4ns
-    job = qmm.simulate(config, cryoscope, simulation_config)
-    job.get_simulated_samples().con1.plot()
-else:
-    # Open the quantum machine
-    qm = qmm.open_qm(config)
-    # Send the QUA program to the OPX, which compiles and executes it
-    job = qm.execute(cryoscope)
-    # Get results from QUA program
-    results = fetching_tool(job, ["n", "I1", "Q1", "state1", "I2", "Q2", "state2"], mode="live")
-    # Live plotting
-    fig = plt.figure()
-    interrupt_on_close(fig, job)  #  Interrupts the job when closing the figure
-    while results.is_processing():
-        # Fetch results
-        n, I1, Q1, state1, I2, Q2, state2 = results.fetch_all()
-        # Convert the results into Volts
-        I1, Q1 = u.demod2volts(I1, readout_len), u.demod2volts(Q1, readout_len)
-        I2, Q2 = u.demod2volts(I2, readout_len), u.demod2volts(Q2, readout_len)
-        # Progress bar
-        progress_counter(n, n_avg, start_time=results.start_time)
-        # Bloch vector Sx + iSy
-        if qubit == 1:
-            Sxx = state1[:, 0] * 2 - 1
-            Syy = state1[:, 1] * 2 - 1
-        elif qubit == 2:
-            Sxx = state2[:, 0] * 2 - 1
-            Syy = state2[:, 1] * 2 - 1
-        else:
-            Sxx = 0
-            Syy = 0
-        S = Sxx + 1j * Syy
-        # Accumulated phase: angle between Sx and Sy
-        phase = np.unwrap(np.angle(S))
-        phase = phase - phase[-1]
-        # Filtering and derivative of the phase to get the averaged frequency
-        detuning = signal.savgol_filter(phase / 2 / np.pi, 21, 2, deriv=1, delta=0.001)
-        # Flux line step response in freq domain and voltage domain
-        step_response_freq = detuning / np.average(detuning[-int(const_flux_len / 2) :])
-        step_response_volt = np.sqrt(step_response_freq)
-        # Plots
-        plt.suptitle(f"Cryoscope for qubit {qubit} (qubit 1 (2) displayed on top (bottom))")
-        plt.subplot(241)
-        plt.cla()
-        plt.plot(xplot, I1)
-        plt.xlabel("Pulse duration [ns]")
-        plt.ylabel("I quadrature [V]")
-        plt.legend(("X", "Y"), loc="lower right")
-        plt.subplot(242)
-        plt.cla()
-        plt.plot(xplot, Q1)
-        plt.xlabel("Pulse duration [ns]")
-        plt.ylabel("Q quadrature [V]")
-        plt.legend(("X", "Y"), loc="lower right")
-        plt.subplot(243)
-        plt.cla()
-        plt.plot(xplot, state1)
-        plt.xlabel("Pulse duration [ns]")
-        plt.ylabel("Excited state population")
-        plt.legend(("X", "Y"), loc="lower right")
-        plt.subplot(244)
-        plt.cla()
-        plt.plot(xplot, step_response_freq, label="Frequency")
-        plt.xlabel("Pulse duration [ns]")
-        plt.ylabel("Step response")
-        plt.title(f"Qubit {qubit}")
-        plt.legend()
-
-        plt.subplot(245)
-        plt.cla()
-        plt.plot(xplot, I2)
-        plt.xlabel("Pulse duration [ns]")
-        plt.ylabel("I quadrature [V]")
-        plt.legend(("X", "Y"), loc="lower right")
-        plt.subplot(246)
-        plt.cla()
-        plt.plot(xplot, Q2)
-        plt.xlabel("Pulse duration [ns]")
-        plt.ylabel("Q quadrature [V]")
-        plt.legend(("X", "Y"), loc="lower right")
-        plt.subplot(247)
-        plt.cla()
-        plt.plot(xplot, state2)
-        plt.xlabel("Pulse duration [ns]")
-        plt.ylabel("Excited state population")
-        plt.legend(("X", "Y"), loc="lower right")
-        plt.subplot(248)
-        plt.cla()
-        plt.plot(xplot, step_response_volt, label=r"Voltage ($\sqrt{freq}$)")
-        plt.xlabel("Pulse duration [ns]")
-        plt.ylabel("Step response")
-        plt.legend()
-        plt.title(f"Qubit {qubit}")
-
-        plt.tight_layout()
-        plt.pause(0.1)
-
-    ## Fit step response with exponential
-    [A, tau], _ = optimize.curve_fit(
-        exponential_decay,
-        xplot,
-        step_response_volt,
-    )
-    print(f"A: {A}\ntau: {tau}")
-
-    ## Derive IIR and FIR corrections
-    fir, iir = filter_calc(exponential=[(A, tau)])
-    print(f"FIR: {fir}\nIIR: {iir}")
-
-    ## Derive responses and plots
-    # Response without filter
-    no_filter = exponential_decay(xplot, A, tau)
-    # Response with filters
-    with_filter = no_filter * signal.lfilter(fir, [1, iir[0]], step_response_th)  # Output filter , DAC Output
-
-    # Plot all data
-    plt.rcParams.update({"font.size": 13})
-    plt.figure()
-    plt.suptitle("Cryoscope with filter implementation")
-    plt.plot(xplot, step_response_volt, "o-", label="Experimental data")
-    plt.plot(xplot, no_filter, label="Fitted response without filter")
-    plt.plot(xplot, with_filter, label="Fitted response with filter")
-    plt.plot(xplot, step_response_th, label="Ideal WF")  # pulse
-    plt.text(
-        max(xplot) // 2,
-        max(step_response_volt) / 2,
-        f"IIR = {iir}\nFIR = {fir}",
-        bbox=dict(boxstyle="round", facecolor="wheat", alpha=0.5),
-    )
-    plt.text(
-        max(xplot) // 4,
-        max(step_response_volt) / 2,
-        f"A = {A:.2f}\ntau = {tau:.2f}",
-        bbox=dict(boxstyle="round", facecolor="wheat", alpha=0.5),
-    )
-    plt.xlabel("Flux pulse duration [ns]")
-    plt.ylabel("Step response")
-    plt.legend(loc="upper right")
-    plt.tight_layout()
-    # Close the quantum machines at the end in order to put all flux biases to 0 so that the fridge doesn't heat-up
-    qm.close()
