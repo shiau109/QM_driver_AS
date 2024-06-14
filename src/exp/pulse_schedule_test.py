db753d4c27bbef3b0a8e53799d1039a3cd34a63f
shiau109 <shiau109@gmail.com>
2024-06-14 11:15:49 +0800

    Move location and use default to simulate

---
+++ src/exp/pulse_schedule_test.py
@@ -0,0 +1,22 @@
+# Import necessary file
+from pathlib import Path
+link_path = Path(__file__).resolve().parent.parent.parent/"application"/"config_api"/"config_link.toml"
+
+from QM_driver_AS.ultitly.config_io import import_config, import_link
+link_config = import_link(link_path)
+config_obj, spec = import_config( link_path )
+
+config = config_obj.get_config()
+qmm, _ = spec.buildup_qmm()
+
+from ab.QM_config_dynamic import initializer
+
+from exp.cryoscope import Cryoscope
+from exp.rofreq_sweep_power_dep import ROFreqSweepPowerDep
+
+my_exp = ROFreqSweepPowerDep(config, qmm)
+my_exp.initializer = initializer(2000,mode='wait')
+my_exp.pulse_schedule_simulation()
+#
+
+my_exp.pulse_schedule_simulation(["con1"],20000)
