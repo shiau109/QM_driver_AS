5236eb6b029738f08fe2b633f7ce68f4fa956aec
shiau109 <shiau109@gmail.com>
2024-06-16 10:29:19 +0800

    Update S2_find_dispersive_limit_power.py
    New class method

--- application/experiment_method/S2_find_dispersive_limit_power.py
+++ application/experiment_method/S2_find_dispersive_limit_power.py
@@ -16,36 +16,31 @@ from exp.save_data import save_nc, save_fig
 import matplotlib.pyplot as plt
 
 # Set parameters
-init_macro = initializer(10000,mode='wait')
-ro_elements = ["q0_ro", "q1_ro", "q2_ro", "q3_ro", "q4_ro"]
-
+from exp.rofreq_sweep_power_dep import ROFreqSweepPowerDep
+my_exp = ROFreqSweepPowerDep(config, qmm)
+my_exp.initializer = initializer(10000,mode='wait')
+my_exp.ro_elements = ["q0_ro", "q1_ro", "q2_ro", "q3_ro", "q4_ro"]
+my_exp.freq_range = (-15,15)
+my_exp.freq_resolution = 0.1
+my_exp.amp_ratio = (0.1,1.9) # tha value range >0, <2
+my_exp.amp_scale = "lin"
+dataset = my_exp.run( 100 )
 
 save_data = True
-file_name = f"power_dep_resonator_{ro_elements[0]}"
+file_name = f"power_dep_resonator_{my_exp.ro_elements[0]}_{len(my_exp.ro_elements)}"
 save_dir = link_config["path"]["output_root"]
-
-n_avg = 100  # The number of averages
-freq_range = (-15,15)
-freq_resolution = 0.1
-amp_max_ratio = 1.5
-amp_resolution = 0.02
-amp_scale = "lin"   # "lin", "log"
-
-from exp.rofreq_sweep_power_dep import *
-dataset = frequency_sweep_power_dep( ro_elements, config, qmm, n_avg=n_avg, freq_range=freq_range, freq_resolution=freq_resolution, amp_resolution=amp_resolution, amp_max_ratio=amp_max_ratio, amp_scale=amp_scale, initializer=init_macro)  
-
-
 
 if save_data: save_nc( save_dir, file_name, dataset)
 
 
 # Plot
+from exp.rofreq_sweep_power_dep import plot_power_dep_resonator
 dfs = dataset.coords["frequency"].values
 amps = dataset.coords["amp_ratio"].values
 
 for ro_name, data in dataset.data_vars.items():
     fig, ax = plt.subplots()
-    plot_power_dep_resonator(dfs, amps, data.values, ax, amp_scale)
+    plot_power_dep_resonator(dfs, amps, data.values, ax, my_exp.amp_scale)
     ax.set_title(ro_name)
     ax.set_xlabel("additional IF freq (MHz)")
     ax.set_ylabel("amp scale")
