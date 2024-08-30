39ce709fdff0714d390e72332af924d58cf54a01
shiau109 <shiau109@gmail.com>
2024-08-30 15:41:51 +0800

    Update update XY.py

--- application/config_api/update XY.py
+++ application/config_api/update XY.py
@@ -10,32 +10,63 @@ from qspec.update import update_controlFreq, update_controlWaveform
 
 import numpy as np
 
-xy_infos = [
-    {
-        "name":"q0",
-        "q_freq": 4.65, # GHz
-        "LO": 4.8, # GHz
-        "pi_amp": 0.2,
+xy_infos = {
+
+# e.g.
+# "q0":{   
+#     "q_freq": 3.05,   # GHz
+#     "LO": 3.0,        # GHz
+#     "pi_amp": 0.15,   # factor
+#     "pi_len": 40,     # ns 
+#     "90_corr": 1
+# },
+
+    "q0":{   # unfinish
+        "q_freq": 3.05,     # GHz
+        "LO": 3.0,          # GHz
+        "pi_amp": 0.15,
         "pi_len": 40,
         "90_corr": 1
     },
-    {
-        "name":"q3",
-        "q_freq": 4.8, # GHz
-        "LO": 4.85, # GHz
-        "pi_amp": 0.2,
+    "q1":{   
+        "q_freq": 4.049,    # GHz
+        "LO": 4.2,          # GHz
+        "pi_amp": 0.15*0.58,
         "pi_len": 40,
         "90_corr": 1
-    },{
-        "name":"q4",
-        "q_freq": 4.9547 +0.12e-3, # GHz
-        "LO": 5.10, # GHz
-        "pi_amp": 0.2 *1.12 *1.26 *0.976 *0.95,
+    },
+    "q2":{   # unfinish, maybe low T1
+        "q_freq": 4.79, # GHz
+        "LO": 4.7, # GHz
+        "pi_amp": 0.5,
+        "pi_len": 1000,
+        "90_corr": 1,
+        # "wf": "sin",
+    },
+    "q3":{   
+        "q_freq": 3.627, # GHz
+        "LO": 3.7, # GHz
+        "pi_amp": 0.5,
         "pi_len": 40,
-        "90_corr": 1 *1.05
-    }
-    
-]
+        "90_corr": 1
+    },
+    "q4":{   # unfinish, maybe low T1
+        "q_freq": 4.036, # GHz
+        "LO": 4.2, # GHz
+        "pi_amp": 0.5,
+        "pi_len": 40,
+        "90_corr": 1
+    },
+    "q5":{   # unfinish, maybe low T1
+        "q_freq": 4.45, # GHz
+        "LO": 4.4, # GHz
+        "pi_amp": 0.5,
+        "pi_len": 40,
+        "90_corr": 1
+    },
+}    
+updating_qubit = ["q0","q3"]
+
 # name, q_freq(GHz), LO(GHz), amp, len, half
 # update_info = [['q1', 4.526-0.005, 4.60, 0.2*0.9*1.05*1.01, 40, 1.03 ]]
 for i in updating_qubit:
@@ -56,7 +87,12 @@ for i in updating_qubit:
         print("Warning IF > +/-350 MHz, IF is set 350 MHz")
         ref_IF = np.sign(ref_IF)*350
 
-    spec.update_aXyInfo_for(target_q=q_name, amp=pi_amp, len=pi_len, half=i["90_corr"])
+    spec.update_aXyInfo_for(target_q=q_name, 
+                            amp=pi_amp, 
+                            len=pi_len, 
+                            half=xy_infos[i]["90_corr"], 
+                            wf=qubit_wf
+                            )
     update_controlWaveform(config_obj, spec.get_spec_forConfig("xy"), target_q=q_name )
 
 from QM_driver_AS.ultitly.config_io import output_config
