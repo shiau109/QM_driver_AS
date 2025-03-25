48cde96d60b6ad9016c2c5b995186653f3863368
b08202012 <boris8040@gmail.com>
2025-03-25 16:11:15 +0800

    tomography update
    update two qubit tomography and plot density matrix

--- application/experiment_method/tomography.py
+++ application/experiment_method/tomography.py
@@ -15,34 +15,69 @@ from exp.save_data import save_nc, save_fig
 
 import matplotlib.pyplot as plt
 from qm.qua import *
+import numpy as np
 
 # Set parameters
 ro_element = ["q2_ro","q3_ro","q4_ro"]
 q_name = ["q2_xy","q3_xy","q4_xy"]
 n_avg = 1000
-threshold = 5.36e-5
+
 
 def prepare_state():
-
-    play("x180", "q4_xy" )
+    
+    
+    play("x180","q2_xy")
+    align()
+    play("y90", "q2_xy")
+    align()
+    play("x180","q3_xy")
+    align()
+    play("y90", "q3_xy" )
+    align()
+    play("const" * amp(-0.046),"q3_z", duration=25)
+    play("const"*amp(0.30),"q7_z",duration=25)
+    align()
+    frame_rotation_2pi(-3.0718456925739788/(2*np.pi), "q3_xy")
+    frame_rotation_2pi(1.7003101134811742/(2*np.pi), "q2_xy")
+    align()
+    play("x180","q3_xy")
+    align()
+    play("y90", "q3_xy" )
+    #play("x180","q2_xy")
+    #play("x180","q3_xy")
 
     pass
 
-from exp.tomography import StateTomography
+from exp.tomography import StateTomography, calculate_2Q_block_vector, calculate_block_vector, calculate_density_matrix, plot_density_matrix
 my_exp = StateTomography(config, qmm)
-my_exp.ro_elements = ["q4_ro"]
-my_exp.xy_elements = ["q4_xy"]
+my_exp.ro_elements = ["q0_ro","q1_ro","q2_ro","q3_ro","q4_ro"]
+my_exp.xy_elements = ["q3_xy","q2_xy"]
+threshold1 = 3.275e-5#1.647e-4#6.840e-5#4.593e-5
+threshold2 = 1.619e-4#1.434e-5#1.376e-4
 my_exp.process = prepare_state
 dataset = my_exp.run(1000)
 
 save_data = True
 save_dir = link_config["path"]["output_root"]
-save_name = f"{ro_element[0]}{ro_element[1]}_state_tomography"
+save_name = f"{ro_element[1]}{ro_element[2]}_state_tomography"
 
-#from exp.tomography import *
+
 #dataset = state_tomography_NQ(q_name,ro_element,prepare_state,n_avg,config,qmm,simulate=False)
-if save_data: save_nc(save_dir, save_name, dataset) 
+folder_label = "Tomography" #your data and plots will be saved under a new folder with this name
 
-data_i = dataset[ro_element[2]].values[0]#.transpose()
-print(data_i.shape)
-#vect_dis = calculate_block_vector(data_i, threshold)
+if save_data: 
+    from exp.save_data import DataPackager
+    save_dir = link_config["path"]["output_root"]
+    dp = DataPackager( save_dir, folder_label )
+    dp.save_config(config)
+    dp.save_nc(dataset,folder_label) 
+
+data_i1 = dataset[my_exp.ro_elements[2]].values[0].transpose((1,2,0))
+data_i2 = dataset[my_exp.ro_elements[3]].values[0].transpose((1,2,0))
+print(data_i1.shape)
+vect_dis = calculate_2Q_block_vector(data_i1, threshold1, data_i2, threshold2)
+density_matrix = calculate_density_matrix(data_i1, threshold1, data_i2, threshold2)
+print(density_matrix[0,0],density_matrix[1,1],density_matrix[2,2], density_matrix[3,3])
+fig,ax = plt.subplots()
+plot_density_matrix(density_matrix)
+plt.show()
--- src/exp/tomography.py
+++ src/exp/tomography.py
@@ -29,6 +29,7 @@ from qualang_tools.results import progress_counter
 from exp.RO_macros import state_tomo_singleRO_declare, tomo_pre_save_singleShot, state_tomo_measurement, tomo_NQ_proj
 import warnings
 import xarray as xr
+import numpy as np
 
 warnings.filterwarnings("ignore")
 from exp.QMMeasurement import QMMeasurement
@@ -41,7 +42,7 @@ class StateTomography(QMMeasurement):
         self.xy_elements = ["q4_xy"]
         
         self.initializer = None
-        self.process = None
+        #self.process = self.prepare_state
     
     def _get_qua_program(self):
         with program() as tomo:
@@ -51,7 +52,7 @@ class StateTomography(QMMeasurement):
 
             with for_(n, 0, n < self.shot_num, n + 1):
 
-                tomo_NQ_proj( iqdata_stream, self.process, self.xy_elements, self.ro_elements, weights="rotated_", thermalization_time=200)
+                tomo_NQ_proj( iqdata_stream, self.process, self.xy_elements, self.ro_elements, weights="rotated_", thermalization_time=100)
 
                 # Wait for the qubit to decay to the ground state
                 # Save the averaging iteration to get the progress bar
@@ -75,7 +76,7 @@ class StateTomography(QMMeasurement):
         output_data = {}
         coords={"mixer":np.array(["I","Q"]), "shot":np.arange(self.shot_num)}
         dims_order = ["mixer","shot"]
-        for i in range(len(self.ro_elements)):
+        for i in range(len(self.xy_elements)):
             dims_order.append(f"basis{i+1}")
             coords[f"basis{i+1}"] = ["z","x","y"]
 
@@ -88,6 +89,13 @@ class StateTomography(QMMeasurement):
             )
         # Live plotting
         return dataset
+    
+    def process(self):
+        
+        #play("x180", "q3_xy")
+        play("x90", "q4_xy" )
+
+        pass
 
     
 
@@ -228,6 +236,92 @@ def calculate_block_vector( data, threshold ):
         vect_dis.append(pop_0-pop_1)
     vect_dis = [vect_dis[1],vect_dis[2],vect_dis[0]]
     return vect_dis
+
+def calculate_2Q_block_vector(data1, threshold1, data2, threshold2):
+    data1 = np.array(data1)
+    data2 = np.array(data2)
+    vect_dis = []
+    dirction = ["z", "x", "y"]
+    total_count = data1.shape[-1]
+    for idx1, dir1 in enumerate(dirction):
+        for idx2, dir2 in enumerate(dirction):
+            count_0 = np.count_nonzero(np.all([data1[idx1,idx2] < threshold1,data2[idx1,idx2] < threshold2],axis=0))
+            pop_0 = count_0/total_count
+            print(f"{dir1}{dir2} g count {total_count}, {pop_0}")
+
+            pop_1 = 1-pop_0
+            vect_dis.append(pop_0-pop_1)
+    vect_dis = [vect_dis[1],vect_dis[2],vect_dis[0]]
+    return vect_dis
+
+            
+
+def calculate_density_matrix(data1, threshold1, data2, threshold2):
+    data1 = np.array(data1)
+    data2 = np.array(data2)
+    density_matrix = np.zeros((4,4))
+    dirction = ["z", "x", "y"]
+    basis_coeffient_matrix = np.zeros((4,4))
+    basis = ["i","z","x","y"]
+    total_count = data1.shape[-1]
+    for idx1, dir1 in enumerate(dirction):
+        for idx2, dir2 in enumerate(dirction):
+            count_00 = np.count_nonzero(np.all([data1[idx1,idx2] <= threshold1,data2[idx1,idx2] <= threshold2],axis=0))
+            count_10 = np.count_nonzero(np.all([data1[idx1,idx2] > threshold1,data2[idx1,idx2] <= threshold2],axis=0))
+            count_01 = np.count_nonzero(np.all([data1[idx1,idx2] <= threshold1,data2[idx1,idx2] > threshold2],axis=0))
+            count_11 = np.count_nonzero(np.all([data1[idx1,idx2] > threshold1,data2[idx1,idx2] > threshold2],axis=0))
+            pop_00 = count_00/total_count
+            pop_10 = count_10/total_count
+            pop_01 = count_01/total_count
+            pop_11 = count_11/total_count
+            if dir1 != dir2:
+                basis_coeffient_matrix[idx1+1,idx2+1] = pop_00-pop_10-pop_01+pop_11
+            else:
+                basis_coeffient_matrix[idx1+1,idx2+1] = pop_00-pop_10-pop_01+pop_11
+                basis_coeffient_matrix[0,idx2+1] = pop_00+pop_10-pop_01-pop_11
+                basis_coeffient_matrix[idx1+1,0] = pop_00-pop_10+pop_01-pop_11
+    basis_coeffient_matrix[0,0] = 1
+
+    pauli_x = np.array([[0,1],[1,0]])
+    pauli_y = np.array([[0,-1j],[1j,0]])
+    pauli_z = np.array([[1,0],[0,-1]])
+    idendity = np.eye(2)
+    single_q_basis = [idendity, pauli_z, pauli_x, pauli_y]
+    two_q_basis = []
+    for basis_1 in single_q_basis:
+        for basis_2 in single_q_basis:
+            two_q_basis.append(np.kron(basis_1, basis_2))
+    two_q_basis = np.array(two_q_basis).reshape((4,4,4,4))
+    for idx1, dir1 in enumerate(basis):
+        for idx2, dir2 in enumerate(basis):
+            density_matrix = density_matrix+basis_coeffient_matrix[idx1,idx2]*two_q_basis[idx1,idx2]
+    density_matrix = 1/4*density_matrix
+
+    return density_matrix
+
+def plot_density_matrix(density_matrix):
+    #from qutip.visualization import matrix_histogram_complex
+    fig = plt.figure(figsize=(6,6))
+    ax = plt.subplot(projection='3d')
+    density_matrix_1D = density_matrix.reshape((16,))
+    import matplotlib.cm as cm
+    from matplotlib.colors import Normalize
+    norm = Normalize(vmin=-1, vmax=1)
+    cmap = cm.get_cmap('hsv')
+    rgba = [cmap((np.angle(k)+np.pi)/(2*np.pi)) for k in density_matrix_1D]
+    x = np.array([1,3,5,7]*4)
+    y = np.array([1]*4+[3]*4+[5]*4+[7]*4)
+    z = 0
+    phase_map = np.abs(np.angle(density_matrix))/(2*np.pi)
+    ax.bar3d(x,y,z,dx=1,dy=1,dz=np.abs(density_matrix_1D),color=rgba)
+    plt.xticks(ticks=[1,3,5,7],labels=["|00>","|01>","|10>","|11>"])
+    plt.yticks(ticks=[1,3,5,7],labels=["|00>","|01>","|10>","|11>"])
+    plt.title("Density matrix of Bell state")
+    sm = cm.ScalarMappable(norm=norm,cmap=cmap)
+    sm.set_array([])
+    print(np.angle(density_matrix))
+    print([np.angle(k) for k in density_matrix_1D])
+    plt.colorbar(sm,ax=ax).set_label("Phase (pi)")
 
 def plot_block_vector( vect_dis, fig=None ):
     from qutip import Bloch
